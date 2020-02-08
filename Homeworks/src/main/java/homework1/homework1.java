a494cfe4d5cb3a5cb437295c8ecb96598cf867f7
Intiqam <nagiyevintiqam@gmail.com>
2020-02-07 19:46:01 -0800

    --added some little corrections

--- Homeworks/src/main/java/homework1/homework1.java
+++ Homeworks/src/main/java/homework1/homework1.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 
 public class homework1 {
     public static void main(String[] args) {
-        int x = new Random().nextInt(100);
+        int hiddenNum = new Random().nextInt(100);
         Scanner scanner = new Scanner(System.in);
-        int [] arr = new int[101];
-        System.out.println("hidden number: "+x);
+        int[] arr = new int[101];
+        System.out.println("hidden number: " + hiddenNum);
         System.out.print("Please enter your name:");
         String playerName = scanner.next();
         System.out.println("Let the game begin!");
-        int i = 0,countOfEnteredNums=0;
-        boolean flag = true;
-        while (flag) {
+        int i = 0, countOfEnteredNums = 0;
+
+        while (true) {
             countOfEnteredNums++;
             System.out.print("enter num:  ");
-            int y = scanner.nextInt();
+            int playersNum = scanner.nextInt();
 
-            arr[i] = y;
+            arr[i] = playersNum;
             i++;
-            if (y == x) {
-
-                System.out.println("Congratulations " + playerName+"!");
+            if (playersNum == hiddenNum) {
+                System.out.println("Congratulations " + playerName + "!");
                 System.out.print("Your numbers:");
 
-                for (int j = 0; j <countOfEnteredNums ; j++) {
-                    System.out.print(arr[j]+" ");
+                for (int j = 0; j < countOfEnteredNums; j++) {
+                    System.out.print(arr[j] + " ");
                 }
-                flag = false;
-            } else if (x > y) {
+
+                break;
+            } else if (hiddenNum > playersNum) {
                 System.out.println("Your number is too small. Please, try again.");
             } else {
                 System.out.println("Your number is too big. Please, try again.");
