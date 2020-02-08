c4ab64980572f77cc7a048b89281edc623ed5077
Intiqam <nagiyevintiqam@gmail.com>
2020-02-09 02:16:25 +0400

    --added litle correction

--- Homeworks/src/main/java/warmup/warmup.java
+++ Homeworks/src/main/java/warmup/warmup.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 public class warmup {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, what is your name?");
-        String name=scanner.next();
-        System.out.println("Hello "+name);
-        System.out.println("Nice to meet you, "+name);
-        System.out.println("Bye!");
+        String name = scanner.next();
+        System.out.println("Hello "+name+"\nNice to meet you, "+name+"\ngjcBye!");
     }
 }
