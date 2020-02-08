6e431fa5d10f216060e3095286019d30c74e500e
Intiqam <nagiyevintiqam@gmail.com>
2020-02-09 03:35:54 +0400

    --added new line about age

--- Homeworks/src/main/java/warmup/warmup.java
+++ Homeworks/src/main/java/warmup/warmup.java
@@ -8,5 +8,13 @@ public class warmup {
         System.out.println("Hello, what is your name?");
         String name = scanner.next();
         System.out.println("Hello "+name+"\nNice to meet you, "+name+"\nBye!");
+        System.out.println("please enter your age ");
+        int age = scanner.nextInt();
+
+        if (age < 18) {
+            System.out.println("go can to cinema");
+        } else {
+            System.out.println("you can go to night club");
+        }
     }
 }
