863dc75dc22392f272e7e16b06e03445b58cf183
Intiqam <nagiyevintiqam@gmail.com>
2020-02-11 10:57:04 +0400

    --lesson 3 warmUp

--- Homeworks/src/main/java/warmup/lesseon3warmUp.java
+++ Homeworks/src/main/java/warmup/lesseon3warmUp.java
@@ -3,26 +3,29 @@ package warmup;
 import java.util.Scanner;
 
 public class lesseon3warmUp {
+    private static boolean isShoulBeFill(int i, int height, int width, int j, int tan) {
+        return i == 0 || i == height - 1 ||
+                j == 0 || j == width - 1 || j == (i * tan) || (width - 1 - j) == i * tan;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter width:");
+        System.out.print("enter width:");
         int width = scanner.nextInt();
-        System.out.println("enter height:");
-        int height = scanner.nextInt();
+        System.out.print("enter height:");
 
+        StringBuilder envelope = new StringBuilder();
+        int height = scanner.nextInt();
+        int tan = (width - 1) / (height - 1);
         for (int i = 0; i < height; i++) {
-            System.out.println();
             for (int j = 0; j < width; j++) {
-                if (i == 0 || i == height - 1 ||
-                        j == 0 || j == width - 1) {
-                    System.out.print("*");
-                } else if (i == j) {
-                    System.out.print("*");
-                } else if (i + j == width - 1) {
-                    System.out.print("*");
+                if (isShoulBeFill(i, height, width, j, tan)) {
+                    envelope.append("*");
                 } else
-                    System.out.print(" ");
+                    envelope.append(" ");
             }
+            envelope.append("\n");
         }
+        System.out.println(envelope);
     }
 }
